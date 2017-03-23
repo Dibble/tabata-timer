c4faa63f2f63df2068f1c5c4cf9155965f68619f
James Cotterell <james.cotterell@newvoicemedia.com>
2017-03-23 13:55:13 +0000

    Added required files/setup for simple Hello World React app

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: ['./src/main.js'],
+  output: {
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      loader: ['babel-loader'],
+      exclude: /node_modules/,
+      query: {
+        presets: [
+          ['env', { useBuiltIns: true }],
+          'react'
+        ]
+      }
+    }]
+  }
+}
